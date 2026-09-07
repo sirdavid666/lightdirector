2b6119969e7975b9bc4e57e1e495249d6bcca081
sirdavid666 <olujinmiewaoluwa@gmail.com>
2026-09-07 11:51:20 +0100

    Update RTMP fix script for Android manifest and Gradle

--- plugins/withRtmpFix.js
+++ plugins/withRtmpFix.js
@@ -17,7 +17,7 @@ module.exports = function withRtmpFix(config) {
       const androidRoot = path.join(mod.modRequest.platformProjectRoot);
       const libRoot = path.join(mod.modRequest.projectRoot, 'node_modules', 'react-native-nodemediaclient', 'android');
 
-      // 1) Patch library: namespace + android:exported
+      // 1) Patch library manifest: namespace + android:exported
       let pkg = 'com.nodemediaclient';
       const libManifest = path.join(libRoot, 'src', 'main', 'AndroidManifest.xml');
       if (fs.existsSync(libManifest)) {
@@ -27,23 +27,30 @@ module.exports = function withRtmpFix(config) {
         m = m.replace(/<(activity|service|receiver)(?![^>]*android:exported)/g, '$1 android:exported="true"');
         fs.writeFileSync(libManifest, m, 'utf8');
       }
+
+      // 2) Patch library build.gradle: namespace + compileSdk/targetSdk/minSdk (the actual fix for the Gradle error)
       const gradlePath = path.join(libRoot, 'build.gradle');
       if (fs.existsSync(gradlePath)) {
         let g = fs.readFileSync(gradlePath, 'utf8');
         if (!/namespace/.test(g)) {
           g = g.replace(/android\s*{/, "android {\n    namespace '" + pkg + "'");
-          fs.writeFileSync(gradlePath, g, 'utf8');
         }
+        if (!/compileSdk/.test(g)) {
+          g = g.replace(/android\s*{/, 'android {\n    compileSdkVersion 34\n    buildToolsVersion "34.0.0"');
+        }
+        if (!/defaultConfig[\s\S]*?minSdk/.test(g)) {
+          g = g.replace(/defaultConfig\s*{/, 'defaultConfig {\n        minSdkVersion 21\n        targetSdkVersion 34');
+        }
+        fs.writeFileSync(gradlePath, g, 'utf8');
       }
 
-      // 2) Add foreground-service permissions to the app manifest (safe string edit)
+      // 3) Add foreground-service permissions to the app manifest
       const appManifest = path.join(androidRoot, 'app', 'src', 'main', 'AndroidManifest.xml');
       if (fs.existsSync(appManifest)) {
         let am = fs.readFileSync(appManifest, 'utf8');
         let added = false;
         PERMS.forEach((p) => {
           if (!am.includes(p)) {
-            am = am.replace('<manifest', '<manifest\n    xmlns:tools="http://schemas.android.com/tools"');
             am = am.replace(/<manifest([^>]*)>/, '$&\n    <uses-permission android:name="' + p + '" />');
             added = true;
           }
