cae741d2c1e5a5128b617a356bae639b102a639b
sirdavid666 <olujinmiewaoluwa@gmail.com>
2026-09-07 19:17:48 +0100

    Add CompositorView class for SurfaceView management

---
+++ native-src/com/lightdirector/native-src/com/lightdirector/CompositorView.java
@@ -0,0 +1,29 @@
+herepackage com.lightdirector;
+
+import android.content.Context;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.view.ViewGroup.LayoutParams;
+
+public class CompositorView extends FrameLayout {
+private SurfaceView previewView;
+
+public CompositorView(Context context) {
+    super(context);
+    previewView = new SurfaceView(context);
+    previewView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+    addView(previewView);
+}
+
+public View getPreviewView() {
+    return previewView;
+}
+
+public void release() {
+    if (previewView != null) {
+        removeView(previewView);
+        previewView = null;
+    }
+}
+}
